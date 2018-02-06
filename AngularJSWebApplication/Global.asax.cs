773ec672f7eb31cd985419737c29c51dbaf8e95c
Charles Gu <guchaojun1@hotmail.com>
2018-02-07 00:37:45 +0800

    Signed-off-by: Charles Gu <guchaojun1@hotmail.com>

---
+++ AngularJSWebApplication/Global.asax.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace AngularJSWebApplication
+{
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            //WebApiConfig.Register(GlobalConfiguration.Configuration);
+            //Web API,启动特性路由
+            GlobalConfiguration.Configure(WebApiConfig.Register);//必须的
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
